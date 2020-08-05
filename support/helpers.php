ca26a9d840cc1f023ab11f44e3cf52696d08a66d
Eduardo Guerra <eduardo.antares@gmail.com>
2020-08-05 22:12:04 +0000

    Add SupportTest

--- support/helpers.php
+++ support/helpers.php
@@ -1,25 +1,25 @@
 <?php
 
-if (!function_exists('stringhandler_infos')) {
+if (!function_exists('ai_stringhandler_infos')) {
     /**
      * Get package infos.
      *
-     * @return array
+     * @return object
      */
-    function stringhandler_infos()
+    function ai_stringhandler_infos()
     {
-        return json_decode(file_get_contents(stringhandler_path('support/infos.json')));
+        return json_decode(file_get_contents(ai_stringhandler_path('support/infos.json')));
     }
 }
 
-if (!function_exists('stringhandler_path')) {
+if (!function_exists('ai_stringhandler_path')) {
     /**
      * Return the path of the resource relative to the package
      *
      * @param string $resource
      * @return string
      */
-    function stringhandler_path($resource = null)
+    function ai_stringhandler_path($resource = null)
     {
         if (!empty($resource) and substr($resource, 0, 1) != DIRECTORY_SEPARATOR) {
             $resource = DIRECTORY_SEPARATOR . $resource;
